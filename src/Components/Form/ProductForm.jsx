9c8fa0478351620ab76f4f861c8f76f70d335eee
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-04-29 17:26:57 -0700

    Update

--- src/Components/Form/ProductForm.jsx
+++ src/Components/Form/ProductForm.jsx
@@ -3,50 +3,50 @@ import { useForm } from 'react-hook-form'
 import { addProduct } from '../../Services/ProductServices';
 
 export default function ProductForm() {
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
-    
-    const onSubmit = async (data) => {
-      try {
-        await addProduct(data)
-         console.log(data)
-         reset()
-          alert('Product added successfully')
-          window.location.href = "/";
-        } catch (error) {
-        console.error(error)
-      }
-      
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      await addProduct(data)
+      console.log(data)
+      reset()
+      alert('Product added successfully')
+      window.location.href = "/";
+    } catch (error) {
+      console.error(error)
     }
-    
+
+  }
+
   return (
-<>
-        <section className="my-20 max-w-4xl p-6 mx-auto bg-black rounded-lg shadow-md dark:bg-gray-800 ">
+    <>
+      <section className="my-20 max-w-4xl p-6 mx-auto bg-black rounded-lg shadow-md dark:bg-gray-800 ">
         <h1 className="text-xl font-bold text-white capitalize dark:text-white text-center">Add Products</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-white dark:text-gray-200">Product Name</label>
-              <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" {...register("Name", { required: true })}/>
+              <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" {...register("Name", { required: true })} />
               {errors.Name && <span>required</span>}
             </div>
             <div>
               <label className="text-white dark:text-gray-200">Price</label>
-              <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" {...register("Price", { required: true })}/>
+              <input type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" {...register("Price", { required: true })} />
               {errors.Price && <span>required</span>}
             </div>
             <div>
               <label className="text-white dark:text-gray-200" >Qty</label>
-              <input type="number" className="block w-full px-4 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" {...register("Qty", { required: true })}/>
+              <input type="number" className="block w-full px-4 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" {...register("Qty", { required: true })} />
               {errors.Qty && <span>required</span>}
             </div>
             <div>
               <label className="text-white dark:text-gray-200">Description</label>
-              <textarea  type="textarea" className="block w-full h-44 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"{...register("Description", { required: true })}></textarea>
+              <textarea type="textarea" className="block w-full h-44 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"{...register("Description", { required: true })}></textarea>
               {errors.Description && <span>required</span>}
             </div>
             <div className="">
@@ -59,14 +59,14 @@ export default function ProductForm() {
                     <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                       <span className="">Upload a file</span>
                       <input
-    id="file-upload"
-    name="file-upload"
-    type='file'
-    className="sr-only"
-    accept="image/*"
-   
-    {...register("ProductIMG", { required: true })}
-/>
+                        id="file-upload"
+                        name="file-upload"
+                        type='file'
+                        className="sr-only"
+                        accept="image/*"
+
+                        {...register("ProductIMG", { required: true })}
+                      />
 
                       {errors.ProductIMG && <span>required</span>}
 
@@ -79,7 +79,7 @@ export default function ProductForm() {
               </div>
             </div>
           </div>
-                
+
 
           <div className="flex justify-end mt-6">
             <button type='submit' className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-950 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">Post</button>
@@ -89,6 +89,6 @@ export default function ProductForm() {
 
 
 
-</>  )
+    </>)
 }
 
